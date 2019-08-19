a3338bef55e3cc0cb902b1ac4b54bd4969d7f956
JulienPlume <julien.plume@gmail.com>
2019-08-19 22:26:33 +0200

    init pre-version

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "Apple";
+  src: url("assets/fonts/Apple.ttf");
+}
+body{
+    /* background-color: black; */
+    margin: 0 auto;
+}
+#app {
+  font-family: 'Apple';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  /* margin-top: 60px; */
+  /* font-size: 2.5%; */
+
+
+
+}
+</style>
